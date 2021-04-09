aa89fe5be6d660533e08d64e906591826a0e4aaf
samriz <samriz@users.noreply.github.com>
2021-04-09 12:39:21 -0500

    Made TestController a partial class and placed "helper" functions in separate file.

--- InterviewTest/Controllers/TestController.cs
+++ InterviewTest/Controllers/TestController.cs
@@ -12,7 +12,7 @@ using System.Web.Mvc;
 
 namespace InterviewTest.Controllers
 {
-    public class TestController : Controller
+    public partial class TestController : Controller
     {
         public ActionResult Index(){return View();}
 
@@ -55,78 +55,6 @@ namespace InterviewTest.Controllers
             //return ar;
             List<PartTwoBModel> p2bList = await UsingLINQAsync();
             return View(p2bList);
-        }
-
-        //public async Task<ActionResult> UsingLINQAsync()
-        public async Task<List<PartTwoBModel>> UsingLINQAsync()
-        {
-            InterviewTestEntities interviewTestEntities = new InterviewTestEntities();
-            IEnumerable<Employee> employees = interviewTestEntities.Employees;
-            IEnumerable<Office> offices = interviewTestEntities.Offices;
-            IEnumerable<Position> positions = interviewTestEntities.Positions;
-            
-            List<PartTwoBModel> p2bList = new List<PartTwoBModel>();
-
-            Task execute = Task.Run(() =>
-            {
-                var employeesAndTheirOfficesAndPositions =
-
-                    from employee in employees
-                    join office in offices on employee.officeId equals office.id
-                    join position in positions on employee.positionId equals position.id
-                    select new
-                    {
-                        employee.firstName,
-                        employee.lastName,
-                        office.officeName,
-                        position.position
-                    };
-                //convert LINQ query IEnumerable to a List and iterate over it
-                foreach(var item in employeesAndTheirOfficesAndPositions.ToList())
-                {
-                    //add new object to p2bList and assign each object properties to that of employeesAndTheirOfficesAndPositions
-                    p2bList.Add(new PartTwoBModel() 
-                    {
-                        firstName = item.firstName,
-                        lastName = item.lastName,
-                        officeName = item.officeName,
-                        position = item.position
-                    });
-                }
-            });
-            await execute;
-            return p2bList;
-            //return View(p2bList);
-        }
-
-        public async Task<ActionResult> NotLINQAsync()
-        {
-            InterviewTestEntities interviewTestEntities = new InterviewTestEntities();
-            IEnumerable<Employee> employees = interviewTestEntities.Employees;
-            IEnumerable<Office> offices = interviewTestEntities.Offices;
-            IEnumerable<Position> positions = interviewTestEntities.Positions;
-
-            string connectionString = @"metadata=res://*/Models.InterviewTestEntities.csdl|res://*/Models.InterviewTestEntities.ssdl|res://*/Models.InterviewTestEntities.msl;provider=System.Data.SqlClient;provider connection string=&quot;data source=(LocalDB)\MSSQLLocalDB;attachdbfilename=|DataDirectory|\InterviewTest.mdf;integrated security=True;connect timeout=30;MultipleActiveResultSets=True;App=EntityFramework&quot;";
-
-            //create SQL connection
-            SqlConnection sqlConnection = new SqlConnection(connectionString);
-
-            //SQL query
-            string query = @"select Employee.firstName, Employee.lastName, Office.officeName, Position.position from Employee inner join Office on Employee.officeId = Office.id inner join Position on Position.id = Employee.positionId";
-
-            //build command that will execute SQL query
-            SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
-
-            //open connection prior to executing command
-            sqlConnection.Open();
-
-            //execute command
-            int numberOfRowsAffected = await sqlCommand.ExecuteNonQueryAsync();
-            if(numberOfRowsAffected > 0)
-            {
-
-            }
-            return View();
         }
 
         [HandleError(View = "PartThreeError")]
---
+++ InterviewTest/Controllers/TestControllerHelperFunctions.cs
@@ -0,0 +1,98 @@
+﻿using InterviewTest.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity.Validation;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InterviewTest.Controllers
+{
+    public partial class TestController : Controller
+    {
+        //public async Task<ActionResult> UsingLINQAsync()
+        public async Task<List<PartTwoBModel>> UsingLINQAsync()
+        {
+            InterviewTestEntities interviewTestEntities = new InterviewTestEntities();
+            IEnumerable<Employee> employees = interviewTestEntities.Employees;
+            IEnumerable<Office> offices = interviewTestEntities.Offices;
+            IEnumerable<Position> positions = interviewTestEntities.Positions;
+            
+            List<PartTwoBModel> p2bList = new List<PartTwoBModel>();
+
+            Task execute = Task.Run(() =>
+            {
+                var employeesAndTheirOfficesAndPositions =
+
+                    from employee in employees
+                    join office in offices on employee.officeId equals office.id
+                    join position in positions on employee.positionId equals position.id
+                    select new
+                    {
+                        employee.firstName,
+                        employee.lastName,
+                        office.officeName,
+                        position.position
+                    };
+
+                //user pagination here
+                //Paginate(employeesAndTheirOfficesAndPositions);
+
+                //convert LINQ query IEnumerable to a List and iterate over it
+                foreach(var item in employeesAndTheirOfficesAndPositions.ToList())
+                {
+                    //add new object to p2bList and assign each object properties to that of employeesAndTheirOfficesAndPositions
+                    p2bList.Add(new PartTwoBModel() 
+                    {
+                        firstName = item.firstName,
+                        lastName = item.lastName,
+                        officeName = item.officeName,
+                        position = item.position
+                    });
+                }
+            });
+            await execute;
+            return p2bList;
+            //return View(p2bList);
+        }
+
+        private void Paginate(IEnumerable<dynamic> queryList, int offset)
+        {
+           // from item in queryList orderby item.firstName 
+        }
+
+        public async Task<ActionResult> NotLINQAsync()
+        {
+            InterviewTestEntities interviewTestEntities = new InterviewTestEntities();
+            IEnumerable<Employee> employees = interviewTestEntities.Employees;
+            IEnumerable<Office> offices = interviewTestEntities.Offices;
+            IEnumerable<Position> positions = interviewTestEntities.Positions;
+
+            string connectionString = @"metadata=res://*/Models.InterviewTestEntities.csdl|res://*/Models.InterviewTestEntities.ssdl|res://*/Models.InterviewTestEntities.msl;provider=System.Data.SqlClient;provider connection string=&quot;data source=(LocalDB)\MSSQLLocalDB;attachdbfilename=|DataDirectory|\InterviewTest.mdf;integrated security=True;connect timeout=30;MultipleActiveResultSets=True;App=EntityFramework&quot;";
+
+            //create SQL connection
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+            //SQL query
+            string query = @"select Employee.firstName, Employee.lastName, Office.officeName, Position.position from Employee inner join Office on Employee.officeId = Office.id inner join Position on Position.id = Employee.positionId";
+
+            //build command that will execute SQL query
+            SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+
+            //open connection prior to executing command
+            sqlConnection.Open();
+
+            //execute command
+            int numberOfRowsAffected = await sqlCommand.ExecuteNonQueryAsync();
+            if(numberOfRowsAffected > 0)
+            {
+
+            }
+            return View();
+        }
+    }
+}
