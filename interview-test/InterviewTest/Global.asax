f5c3e400a3f98c2db92c5d89d1d07a0120de1c17
samriz <samriz@users.noreply.github.com>
2022-05-22 17:49:53 -0500

    Removed unnecessary folder.

--- interview-test/InterviewTest/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="InterviewTest.MvcApplication" Language="C#" %>
--- interview-test/InterviewTest/savedqueries/SQLQuery4.sql
+++
@@ -1,4 +0,0 @@
-﻿--SET IDENTITY_INSERT Applicant OFF
-
-INSERT INTO Applicant(firstName, middleName, lastName, positionId, comments, remote) values ('Bruce', NULL, 'Wayne', '3545', 'test bruce wayne', '0')
-
